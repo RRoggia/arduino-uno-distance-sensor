30efbd8a635df7205a13b1e816f6fa08080b1a25
rroggia <renanzr@gmail.com>
2016-12-08 17:43:26 -0200

    implement leds reaction according to the distance

--- distance/distance.ino
+++ distance/distance.ino
@@ -1,10 +1,17 @@
 #include "Ultrasonic.h"
 const int trigger = 13;
 const int echo = 12;
-
 Ultrasonic ultrasonic(trigger, echo);
 
+const int red = 9;
+const int yellow = 10;
+const int green = 11;
+
+
 void setup() {
+  pinMode(red, OUTPUT);
+  pinMode(yellow, OUTPUT);
+  pinMode(green, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -12,6 +19,20 @@ void loop() {
   int distance = (ultrasonic.Ranging(CM));
   Serial.print("Distance is: ");
   Serial.println(distance);
+
+  if(distance < 20){
+    digitalWrite(red,HIGH);
+    digitalWrite(yellow,LOW);
+    digitalWrite(green,LOW);
+  } else if (distance >= 20 && distance < 40){
+    digitalWrite(red,LOW);
+    digitalWrite(yellow,HIGH);
+    digitalWrite(green,LOW);
+  }else {
+    digitalWrite(red,LOW);
+    digitalWrite(yellow,LOW);
+    digitalWrite(green,HIGH);
+  }
 
   delay(1000);
 
