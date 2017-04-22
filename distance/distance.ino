d1b3e86f7f13bbe91a815736d9a137176fdb7b6e
Renan Roggia <renanzr@gmail.com>
2017-04-22 20:20:44 -0300

    Update distance.ino

--- distance/distance.ino
+++ distance/distance.ino
@@ -3,15 +3,11 @@ const int trigger = 13;
 const int echo = 12;
 Ultrasonic ultrasonic(trigger, echo);
 
-const int red = 9;
-const int yellow = 10;
-const int green = 11;
+const int lamp_relay = 7;
 
 
 void setup() {
-  pinMode(red, OUTPUT);
-  pinMode(yellow, OUTPUT);
-  pinMode(green, OUTPUT);
+  pinMode(lamp_relay, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -20,20 +16,12 @@ void loop() {
   Serial.print("Distance is: ");
   Serial.println(distance);
 
-  if(distance < 20){
-    digitalWrite(red,HIGH);
-    digitalWrite(yellow,LOW);
-    digitalWrite(green,LOW);
-  } else if (distance >= 20 && distance < 40){
-    digitalWrite(red,LOW);
-    digitalWrite(yellow,HIGH);
-    digitalWrite(green,LOW);
-  }else {
-    digitalWrite(red,LOW);
-    digitalWrite(yellow,LOW);
-    digitalWrite(green,HIGH);
+  if(distance < 50){
+    digitalWrite(lamp_relay, HIGH);
+  }else{
+    digitalWrite(lamp_relay, LOW);
   }
 
-  delay(1000);
+  delay(2000);
 
 }
